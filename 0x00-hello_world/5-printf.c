f306983d99de95ef7526dad0edde567fe15c2c43
Bahjaoui <mohamed07bahjaoui@gmail.com>
2023-03-16 17:41:52 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Entry poit
+*return : always 0 (success)
+*/
+int main(void)
+{
+         puts("\"with proper grammar, but the outcome is a piece of art,");
+         return (0);
+}
